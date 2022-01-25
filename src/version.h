a11e14ef0d6c03b09686bb8b9d905896b4700b0d
Gibbon <atsb@atsb>
2022-01-25 11:16:47 +0100

    This is version 3.0.0

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "2.9.1"
+#define VERSIONSTR "3.0.0"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 2,9,1,0
-#define RC_PRODUCTVERSION 2,9,1,0
-#define RC_PRODUCTVERSION2 "2.9.1"
+#define RC_FILEVERSION 3,0,0,0
+#define RC_PRODUCTVERSION 3,0,0,0
+#define RC_PRODUCTVERSION2 "3.0.0"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
